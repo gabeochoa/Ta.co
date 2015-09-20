acb87ddfbf2276fae503c7df23b36d49d25a13e9
Gabe <gabe1118@yahoo.com>
2015-09-20 02:50:43 -0400

    Added Yelp

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, redirect, url_for, session, request, render_template
+import yelp
+import keys
 
 app = Flask(__name__)
 app.debug = True
+
+yelp_api = yelp.Api(consumer_key=MY_CONSUMER_KEY,
+                    consumer_secret=MY_CONSUMER_SECRET,
+                    access_token_key=MY_ACCESS_TOKEN,
+                    access_token_secret=MY_ACCESS_SECRET)
+
 
 @app.route('/')
 def index():
@@ -16,7 +24,12 @@ def map_place(place=None):
     return render_template('map.html', place=place, output=output)
 
 def get_food(place):
-    return []
+    search_results = yelp_api.Search(term="my search term", location="my location") # location and search term are required
+    places = []
+    for business in search_results.businesses:
+        print business.name
+        places.append(business.name)
+    return places
 
 if __name__ == '__main__':
     app.run()
